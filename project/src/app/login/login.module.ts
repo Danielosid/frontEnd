296d18cf2e61979866e412e57f1ab7deeda7ed16
Teodor Daniel Mocanu <mocanudan99@yahoo.com>
2022-01-25 16:39:28 +0200

    Added login form

--- project/src/app/login/login.module.ts
+++ project/src/app/login/login.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { Router, RouterModule, Routes } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 const router : Routes = [{
   path: 'sign-in',
@@ -11,7 +15,11 @@ const router : Routes = [{
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(router)
+    RouterModule.forChild(router),
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
   declarations: [LoginComponent]
 })
